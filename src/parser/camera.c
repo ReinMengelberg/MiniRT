e5249bbdb356dea872fecf894c3f86af7eed0d30
Reinhard Mengelberg <rmengelb@f0r3s26.codam.nl>
2025-11-02 12:57:00 +0100

    fixed camera parsing

--- src/parser/camera.c
+++ src/parser/camera.c
@@ -1,63 +1,53 @@
 #include "renderclanker.h"
 
-bool add_cylinder(composition *comp, char *line) {
-    char    **tokens;
-    object  *new_obj;
-    object  *current;
-    
-    tokens = ft_split(line, ' ');
-    if (token_count(tokens) != 6 || !check_token(tokens[0], "cy")) {
-        free_array(tokens);
-        return (perror("Incorrect cylinder definition in .rt file"), false);
-    }
-    
-    // Create new object node
-    new_obj = malloc(sizeof(object));
-    if (!new_obj) {
-        free_array(tokens);
-        return (perror("Memory allocation failed"), false);
-    }
-    
-    new_obj->type = CYLINDER;
-    new_obj->data = parse_cylinder(tokens);
-    new_obj->next = NULL;
-    new_obj->prev = NULL;
-    
-    // Check if parse_cylinder failed
-    if (!new_obj->data) {
-        free(new_obj);
-        free_array(tokens);
-        return (perror("Failed to parse cylinder"), false);
-    }
-    
+bool add_camera(composition *comp, char *line) {
+	char    **tokens;
 
-	add_object_to_list(comp, new_obj);
-    free_array(tokens);
-    return (true);
+	if (comp->camera != NULL) {
+		(perror("Error: Multiple camera definitions in .rt file"), false);
+	}
+	
+	tokens = ft_split(line, ' ');
+	if (!tokens) {
+		return (perror("Error: Failed to split tokens for camera definitions"), false);
+	}
+	if (token_count(tokens) != 4 || !check_token(tokens[0], "C")) {
+		free_array(tokens);
+		return (perror("Error: Incorrect camera definition in .rt file"), false);
+	}
+	
+	// Create new object node
+	comp->camera = parse_camera(tokens);
+	free_array(tokens);
+	if (!comp->camera) {
+		return false;
+	}
+	return (true);
 }
 
 camera *parse_camera(char **tokens) {
 	camera *cam;
 
-	cam = malloc(sizeof(caminder));
+	cam = malloc(sizeof(camera));
 	if (!cam) {
-		return (perror("Failed to allocate memory for a camera"), NULL);
+		return (perror("Error: Failed to allocate memory for a camera"), NULL);
 	}
 
 	cam->root = fill_vector(tokens[1]);
 	cam->direction = fill_direction(tokens[2]);
 	cam->fov = ft_atoi(tokens[3]);
-	free_array(tokens);
 
-	if (!cam->root || !cam->direction || !cam->radius || !cam->height || !cam->color) {
-		free_object(cam);
-		return (perror("Incorrect camera definition in .rt file"), NULL);
+	if (!cam->root || !cam->direction || cam->fov > 180 || cam->fov < 0) {
+		free_camera(cam);
+		return (perror("Error: Incorrect camera definition in .rt file"), NULL);
 	}	
 
 	return cam;
 }
 
 void free_camera(camera *cam) {
+	if (!cam)
+		return;
 	if (cam->root) free(cam->root);
 	if (cam->direction) free(cam->direction);
 	free(cam);

1a373d116db02fc53e8881098834a61432a7129e
Reinhard Mengelberg <rmengelb@f0r3s26.codam.nl>
2025-11-08 12:18:19 +0100

    images

--- src/main.c
+++ src/main.c
@@ -29,15 +29,31 @@ int	handle_close(mlx_data *data) {
 	return (0);
 }
 
+void    put_pixel(t_img *img, int x, int y, int color) {
+    char    *dst;
+
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+        return;
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+
+t_img create_image(void *mlx, composition *comp) {
+	
+}
+
 int	main(int ac, char **av) {
 	int			fd;
 	composition	*comp;
-	// mlx_data	data;
+	void		*image;
+	void		*mlx;
 
 	if (ac != 2)
 		return (dprintf(2, "Error:Usage: ./miniRT <scene.rt>\n"), 1);
 	if (!ft_strnstr(av[1], ".rt", ft_strlen(av[1])))
 		return (dprintf(2, "Error: File must have .rt extension\n"), 1);
+
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0)
 		return (dprintf(2, "Error: Can't open file\n"), 1);
@@ -46,15 +62,12 @@ int	main(int ac, char **av) {
 	if (!comp)
 		return (dprintf(2, "Error: Failed to parse scene\n"), 1);
 
-	// data.comp = comp;
-	// data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "miniRT");
-	// if (!data.win) {
-	// 	free_composition(comp);
-	// 	return (dprintf(2, "Error: Failed to create window\n"), 1);
-	// }
-	// mlx_key_hook(data.win, handle_keypress, &data);
-	// mlx_hook(data.win, 17, 0, handle_close, &data);
-	// mlx_loop(data.mlx);
+	mlx = mlx_init();
+	if (!mlx)
+		return (dprintf(2, "Error: Failed to init MLX\n"), 1);
+
+	
+
 	free_composition(comp);
 	return (0);
 }

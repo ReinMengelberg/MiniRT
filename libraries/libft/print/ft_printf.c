1285bc27ca0d6fd1a7de552482263b61662dafa8
Reinhard Mengelberg <rmengelb@f1r3s10.codam.nl>
2025-11-29 14:28:40 +0100

    progress

--- libraries/libft/print/ft_printf.c
+++ libraries/libft/print/ft_printf.c
@@ -6,11 +6,38 @@
 /*   By: rmengelb <rmengelb@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/01 17:58:09 by rmengelb      #+#    #+#                 */
-/*   Updated: 2025/11/29 14:25:12 by rmengelb      ########   odam.nl         */
+/*   Updated: 2025/11/29 14:27:57 by rmengelb      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_which_dprint(int fd, char specifier, va_list args)
+{
+	int	pc;
+
+	if (specifier == 'c')
+		pc = ft_dprint_c(fd, va_arg(args, int));
+	else if (specifier == 's')
+		pc = ft_dprint_s(fd, va_arg(args, char *));
+	else if (specifier == 'p')
+		pc = ft_dprint_p(fd, va_arg(args, void *));
+	else if (specifier == 'd')
+		pc = ft_dprint_d(fd, va_arg(args, int));
+	else if (specifier == 'i')
+		pc = ft_dprint_i(fd, va_arg(args, int));
+	else if (specifier == 'u')
+		pc = ft_dprint_u(fd, va_arg(args, unsigned int));
+	else if (specifier == 'x')
+		pc = ft_dprint_x(fd, va_arg(args, unsigned int));
+	else if (specifier == 'X')
+		pc = ft_dprint_xupper(fd, va_arg(args, unsigned int));
+	else if (specifier == '%')
+		pc = ft_dprint_procent(fd);
+	else
+		pc = 0;
+	return (pc);
+}
 
 int	ft_printf(const char *format, ...)
 {
